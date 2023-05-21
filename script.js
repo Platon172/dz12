c1e7877e91911926b0a3b83701bd1f5c66bf780e
Platon172 <107851230+Platon172@users.noreply.github.com>
2023-05-21 22:15:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let numbers = [2, 14, 4, 38, 20];
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 13) {
+    console.log(numbers[i]);
+  }
+} 
+
+
+//2
+
+let n1 = 10;
+let n2 = 20;
+
+for(i = n1; i<n2; i++) {
+  console.log(i)
+}
+
+//3
+
+
+let b1 = prompt();
+let b2 = prompt();
+
+let g1 = Number(b1);
+let g2 = Number(b2);
+
+let g3 = g1 + g2;
+
+console.log(g3)
